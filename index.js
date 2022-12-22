36b32887f0c3bd1a5bc16201362866c6f4ac21ed
rhanda2 <rhanda2@huskers.unl.edu>
2022-12-22 13:57:48 -0600

    Setting up basic get all posts and create post workfows.

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from './routes/posts.js'
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
+app.use('/posts', postRoutes)
+
 const CONNECTION_URL = "mongodb+srv://dev-ritvik:bossman123@cluster0.8vhpcps.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5100;
 
 mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
     
     
